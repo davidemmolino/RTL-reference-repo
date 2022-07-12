14d9fee005ddbe5ee8f15b0074c2b3bd1d9ee938
Davide <davidemmolino@gmail.com>
2022-07-11 18:51:43 -0700

    css reset, removed cra init code, added first tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
+  //render the component to test
   render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  //find element that we're interested in
+  //button with initial text of 'name'
+  const button = screen.getByRole('button', { name: 'Change to blue'});
+
+  expect(button).toHaveStyle({ backgroundColor: 'red'});
+});
+
+test('button has correct initial text', () => {
+
+});
+
+test('button turns blue when clicked', () => {
+
 });
